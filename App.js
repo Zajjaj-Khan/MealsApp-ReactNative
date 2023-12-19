f6cbbeddbe748747a3f5d1078a3e854daa5767d6
Zajjaj-Khan <kzajjaj@gmail.com>
2023-12-19 23:23:46 +0500

    Drawer added

--- App.js
+++ App.js
@@ -1,13 +1,56 @@
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsDetail from "./screens/MealsDetail";
 import MealsOverDetails from "./screens/MealsOverDetails";
+import Favirotes from "./screens/Favirotes";
+
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        backgroundColor: "#342715",
+        headerStyle: { backgroundColor: "#342715" },
+        headerTintColor: "#FFFFFF",
+        sceneContainerStyle: { backgroundColor: "#24180f" },
+        drawerContentStyle:{ backgroundColor: "#342715"},
+        drawerActiveTintColor:"#fff",
+        drawerActiveBackgroundColor:"#92725a",
+        drawerInactiveTintColor:"#ccc"
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{ title: "All Categories", 
+        drawerIcon: ({focused, size}) => (
+          <Ionicons
+             name="md-list"
+             size={size}
+             color={focused ? '#fff' : '#ccc'}
+          />
+       ),
+        }}
+      />
+      <Drawer.Screen name="FavirotesScreen" component={Favirotes} options={{drawerIcon: ({focused, size}) => (
+          <Ionicons
+             name="md-star"
+             size={size}
+             color={focused ? '#fff' : '#ccc'}
+          />
+       ),}} />
+    </Drawer.Navigator>
+  );
+}
 export default function App() {
   return (
     <>
@@ -22,22 +65,13 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
+            name="DrawerScreen"
+            component={DrawerNavigator}
             options={{
-              title: "All Categories",
+              headerShown: false,
             }}
           />
-          <Stack.Screen
-            name="MealsDetail"
-            component={MealsDetail}
-            // options={({ route, navigation }) => {
-            //   const catId = route.params.categoryId;
-            //   return {
-            //     title: catId,
-            //   };
-            // }}
-          />
+          <Stack.Screen name="MealsDetail" component={MealsDetail} />
           <Stack.Screen name="MealsOverDetails" component={MealsOverDetails} />
         </Stack.Navigator>
       </NavigationContainer>
---
+++ screens/Favirotes.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Favirotes = () => {
+  return (
+    <View>
+      <Text>Favirotes</Text>
+    </View>
+  )
+}
+
+export default Favirotes
+
+const styles = StyleSheet.create({})
